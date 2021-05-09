03b2672c50417525facb889a3f1dd6946ab37059
Mehdi <elouakifi.mehdi@gmail.com>
2021-05-09 21:17:09 +0000

    V0.2

--- src/main/java/org/sid/cenima/CenimaApplication.java
+++ src/main/java/org/sid/cenima/CenimaApplication.java
@@ -6,6 +6,16 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
+/*
+*
+* To build the project for production use this command to build the jar
+* cmd: mvnw clean package
+* and tp run it on the server use, you can find the build jar on /target folder
+* cmd : java -jar <theNameOfApplication.jar>
+* */
+
 @SpringBootApplication
 public class CenimaApplication implements CommandLineRunner {
 
